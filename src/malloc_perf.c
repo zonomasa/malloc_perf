116ae74f448d0bcd9bc6d9db120f6da8ff265e25
zonomasa <tatezono@gmail.com>
2014-08-26 23:24:08 +0900

    Create src/malloc_perf.c

---
+++ src/malloc_perf.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Number of sizeArray : 14 */
+int s_sizeArray[] = {8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
+
+void
+malloc_perf(void)
+{
+    for (int i = 0; i < 14; i++){
+
+        
+}
+
+void
+free_perf(void)
+{
+    
+
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    malloc_perf();
+
+    free_perf();
+
+    return 0;
+}
